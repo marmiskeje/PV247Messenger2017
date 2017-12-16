263603aa134df42431f5387ee10486fca15b64e9
Martin Miškeje <manager.programmer@gmail.com>
2017-12-16 17:43:28 +0100

    structure update

--- src/reducers/shared/sharedEventsProcessor.js
+++
@@ -1,9 +0,0 @@
-import * as Events from '../../constants/Events';
-
-export const sharedEventsProcessor = (prevState = { token: null }, event) => {
-    switch (event.type){
-        case Events.CREATE_SESSION_EVENT:
-            return { token: event.payload.token };
-    }
-    return prevState;
-};
